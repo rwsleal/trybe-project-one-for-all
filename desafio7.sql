4b25aa5d9f0637ea25dcd96c65409f47347e5365
Richard Leal <rwsleal@gmail.com>
2022-02-04 16:17:45 -0300

    Adiciona requisito 7

---
+++ desafio7.sql
@@ -0,0 +1,12 @@
+SELECT
+	art.artista,
+  alb.album,
+  COUNT(seg.usuario_id) AS seguidores
+FROM
+	SpotifyClone.artistas AS art
+INNER JOIN
+	SpotifyClone.albuns AS alb ON art.artista_id = alb.artista_id
+INNER JOIN
+	SpotifyClone.seguidores AS seg ON alb.artista_id = seg.artista_id
+GROUP BY artista, album
+ORDER BY seguidores DESC, artista ASC, album ASC
