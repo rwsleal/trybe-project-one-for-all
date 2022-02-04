4d856cff144a40a20f81bb099010f48e2ca545da
Richard Leal <rwsleal@gmail.com>
2022-02-04 14:59:02 -0300

    Adiciona requisito 2

---
+++ desafio2.sql
@@ -0,0 +1,11 @@
+SELECT 
+COUNT(DISTINCT can.cancao_id) AS cancoes,
+COUNT(DISTINCT art.artista_id) AS artistas,
+COUNT(DISTINCT alb.album_id) AS albuns
+FROM
+	SpotifyClone.cancoes AS can
+INNER JOIN
+  SpotifyClone.albuns AS alb ON can.album_id = alb.album_id
+INNER JOIN
+  SpotifyClone.artistas AS art ON alb.artista_id = art.artista_id;
+  
